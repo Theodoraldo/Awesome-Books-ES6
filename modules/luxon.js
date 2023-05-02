146d1254093c5380689102d71d5bd0aa0e3680c3
Theodore Osei Asimeng <theodoraldo@gmail.com>
2023-05-02 09:26:14 +0000

    Update luxon JS file

--- modules/luxon.js
+++ modules/luxon.js
@@ -1,5 +1,5 @@
-import { luxon } from '../node_modules/luxon/src/datetime.js';
-/* Datetime display */
+/* eslint-disable no-undef */
+
 const localDate = luxon.DateTime.local().toLocaleString(luxon.DateTime.DATE_MED);
 const { hour } = luxon.DateTime.local().c;
 const min = luxon.DateTime.local().c.minute;
